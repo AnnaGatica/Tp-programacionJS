296ae827763c41376b75f81c43ac497b57771932
AnnaGatica <166068871+AnnaGatica@users.noreply.github.com>
2024-04-08 07:42:19 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let numero1= 360
+let numero2= 25
+let suma= numero1 + numero2;
+let resta= numero1 - numero2;
+let multiplicacion= numero1 * numero2;
+let division= numero1 / numero2;
+console.log ("La suma es"+ suma)
+console.log ("La resta es"+ resta)
+console.log ("La multiplicacion es"+ multiplicacion)
+console.log ("La division es"+ division)
+
+alert ("La suma es"+ suma);
+alert ("La resta es"+ resta);
+alert ("La multiplicacion es"+ multiplicacion);
+alert ("La division es"+ division);
+
